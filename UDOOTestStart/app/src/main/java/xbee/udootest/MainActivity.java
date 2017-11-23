8bf1ae4153ecfa0a6e121acf52313ba7dd537dc1
Reuben <reuben_ho@hotmail.com>
2017-11-23 13:25:28 +0800

    Added checkoff 3
    added code for checkoff 3, need to add values to arff for training

--- UDOOTestStart/app/src/main/java/xbee/udootest/MainActivity.java
+++ UDOOTestStart/app/src/main/java/xbee/udootest/MainActivity.java
@@ -3,6 +3,8 @@ package xbee.udootest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.hardware.usb.UsbManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.io.BufferedReader;
@@ -17,11 +20,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import me.palazzetti.adktoolkit.AdkManager;
 import weka.classifiers.Classifier;
 import weka.classifiers.lazy.IBk;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
 import weka.core.Instances;
+import wlsvm.WLSVM;
 
 
 public class MainActivity extends Activity {
@@ -38,8 +47,10 @@ public class MainActivity extends Activity {
     private TextView accuracyMeasure;
     private Button checkoff1part2button;
     private TextView confusionMatrix;
-
-
+    private ToggleButton stress_state;
+    private TextView state_result;
+    private Button train;
+    private File svmModel;
     private AdkReadTask mAdkReadTask;
 
     @Override
@@ -61,13 +72,38 @@ public class MainActivity extends Activity {
         accuracyMeasure = (TextView) findViewById(R.id.accuracy);
         confusionMatrix= (TextView) findViewById(R.id.confusionMatrix);
         checkoff1part2button = (Button) findViewById(R.id.checkoff1_2Button);
-
+        checkoff1part2button.setVisibility(View.INVISIBLE);
+        checkoffButton.setVisibility(View.INVISIBLE);
+        stress_state = (ToggleButton) findViewById(R.id.toggleButton_state);
+        state_result = (TextView) findViewById(R.id.textViewresult);
+        state_result.setText("Rest");
+        state_result.setTextColor(Color.GREEN);
+        train = (Button) findViewById(R.id.button_train);
         checkoff1part2button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, Checkoff122.class));
             }
         });
+        WLSVM svmCls = new WLSVM();
+        AssetManager am = getApplicationContext().getAssets();
+        try {
+            InputStream is = am.open("stress_train.arff");
+            InputStreamReader inputStreamReader = new InputStreamReader(is);
+            BufferedReader inputReader= null;
+            inputReader = new BufferedReader(inputStreamReader);
+            Instances data = new Instances(inputReader);
+            data.setClassIndex(data.numAttributes()-1);
+            svmCls.buildClassifier(data);
+            File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            svmModel = new File(root,"svmModel.model");
+
+            // test = svmCls;
+            weka.core.SerializationHelper.write(svmModel.getAbsolutePath(), svmCls);
+            Toast.makeText(getApplicationContext(),"Finished training",Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -183,36 +219,76 @@ public class MainActivity extends Activity {
 
         private boolean running = true;
 
-        public void pause(){
+        public void pause() {
             running = false;
         }
 
         protected Void doInBackground(Void... params) {
 //	    	Log.i("ADK demo bi", "start adkreadtask");
-            while(running) {
-                publishProgress(mAdkManager.readSerial()) ;
+            while (running) {
+                publishProgress(mAdkManager.readSerial());
             }
             return null;
         }
 
         protected void onProgressUpdate(String... progress) {
 
-            float pulseRate= (int)progress[0].charAt(0);
-            float oxygenLvl= (int)progress[0].charAt(1);
-            float pos= (int)progress[0].charAt(2);
+            float pulseRate = (int) progress[0].charAt(0);
+            float oxygenLvl = (int) progress[0].charAt(1);
+            float pos = (int) progress[0].charAt(2);
             int max = 255;
-            if (pulseRate>max) pulseRate=max;
-            if (oxygenLvl>max) oxygenLvl=max;
-            if (pos>max) pos=max;
+            if (pulseRate > max) pulseRate = max;
+            if (oxygenLvl > max) oxygenLvl = max;
+            if (pos > max) pos = max;
 
 //            DecimalFormat df = new DecimalFormat("#.#");
             distance.setText(pulseRate + " (bpm)");
             pulse.setText(oxygenLvl + " (pct)");
             position.setText(pos + "");
+            try {
+                WLSVM svmCls = (WLSVM) weka.core.SerializationHelper.read(svmModel.getAbsolutePath());
+                Attribute Attribute1 = new Attribute("pulseRate");
+                Attribute Attribute2 = new Attribute("oxygenLvl");
+                // Declare the class attribute along with its values (nominal)
+                FastVector fvClassVal = new FastVector(2);
+                fvClassVal.addElement("Rest");
+                fvClassVal.addElement("Stress");
+
+                Attribute ClassAttribute = new Attribute("class", fvClassVal);
+                // Declare the feature vector template
+                FastVector fvWekaAttributes = new FastVector(5);
+                fvWekaAttributes.addElement(Attribute1);
+                fvWekaAttributes.addElement(Attribute2);
+                fvWekaAttributes.addElement(ClassAttribute);
+
+                Instances testingSet = new Instances("TestingInstance", fvWekaAttributes, 1);
+                testingSet.setClassIndex(testingSet.numAttributes() - 1);
+                // Create and fill an instance, and add it to the testingSet
+                Instance iExample = new Instance(testingSet.numAttributes());
+                iExample.setValue((Attribute) fvWekaAttributes.elementAt(0), pulseRate);
+                iExample.setValue((Attribute) fvWekaAttributes.elementAt(1), oxygenLvl);
+                iExample.setValue((Attribute) fvWekaAttributes.elementAt(2), "Rest"); // dummy
+                // add the instance
+                testingSet.add(iExample);
+
+                testingSet.setClassIndex(testingSet.numAttributes() - 1);
+                double pred = -1;
+                for (int i = 0; i < testingSet.numInstances(); i++) {
+                    pred = svmCls.classifyInstance(testingSet.instance(i));
+                }
+                if (pred == 1) {
+                    state_result.setText("Stress");
+                    state_result.setTextColor(Color.RED);
+                } else {
+                    state_result.setText("Rest");
+                    state_result.setTextColor(Color.GREEN);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
     }
-
-
 
 
 }
